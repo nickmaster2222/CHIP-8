f0d9a5dcf31d95571e8824b45505f86e35f7ce69
TheBanana <nickmaster2222@gmail.com>
2022-02-16 16:58:14 -0500

    execute and decode advancements

--- new/decode.sv
+++ new/decode.sv
@@ -24,15 +24,18 @@ module decode(
     input clk,
     input rst,
 	input u16 instruction,//this will be passed to execute as well for multi-clock instructions without this section having to handle it
-	output u4 r1,
-	output u4 r2,
+	input u8 registers[15:0],
+	input u16 I,
+	output u16 r1,
+	output u16 r2,
 	output u8 opcode,
-	output u12 immediate
+	output u12 immediate,
+	output decode_trap //invalid instruction encountered
     );
-	always_comb begin
-		case(instruction) begin
+	always_comb begin//this could be shortened to instead use a LUT for the opcodes and this only handle every input format
+		case(instruction) inside
 			16'h0000: begin 
-				opcode = 0;//NOP
+				opcode = 0;		//NOP
 			end
 			16'h00E0: begin		//CLS
 				opcode = 1;
@@ -42,102 +45,154 @@ module decode(
 			end
 			16'h1???: begin 	//JP addr
 				opcode = 3;
-				immediate = instruction[4:];
+				immediate = instruction[2:0];
 			end
 			16'hB???: begin 	//JP V0, addr
 				opcode = 4;
+				immediate = instruction[2:0]
 			end
 			16'h2???: begin 	//CALL addr
-				opcode = 4;
-				immediate = instruction[4:];
+				opcode = 5;
+				immediate = instruction[2:0];
 			end
 			16'h3???: begin 	//SE Vx, byte
-				opcode = 5;
+				opcode = 6;
+				immediate = instruction[1:0];
+				r1 = registers[instruction[2]];
 			end
 			16'h5???: begin 	//SE Vx, Vy
-				opcode = 5;
+				opcode = 7;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h4???: begin 	//SNE Vx, byte
-				opcode = 6;
+				opcode = 8;
+				immediate = instruction[1:0];
+				r1 = registers[instruction[2]];
 			end
 			16'h9??0: begin 	//SNE Vx, Vy
-				opcode = 6;
+				opcode = 9;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h6???: begin 	//LD Vx, byte
-				opcode = 7;
+				opcode = 10;
+				immediate = instruction[1:0];
+				r1 = registers[instruction[2]];
 			end
 			16'h8??0: begin 	//LD Vx, Vy
-				opcode = 7;
+				opcode = 11;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'hA???: begin 	//LD I, addr
-				opcode = 7;
+				opcode = 10;
+				immediate = instruction[3:0];
+				r1 = 16;//Physical register name for I
 			end
 			16'hF?07: begin 	//LD Vx, DT
-				opcode = 7;
+				opcode = 13;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?0A: begin 	//LD Vx, K
-				opcode = 22;
+				opcode = 14;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?15: begin 	//LD DT, Vx
-				opcode = 7;
+				opcode = 15;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?18: begin 	//LD ST, Vx
-				opcode = 7;
+				opcode = 16;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?29: begin 	//LD F, Vx
-				opcode = 23;
+				opcode = 17;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?33: begin 	//LD B, Vx
-				opcode = 24;
+				opcode = 18;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?55: begin 	//LD [I], Vx
-				opcode = 5;
+				opcode = 19;
+				r1 = registers[instruction[2]];
 			end
 			16'hF?65: begin 	//LD Vx, [I]
-				opcode = 5;
+				opcode = 20;
+				r1 = registers[instruction[2]];
 			end
 			16'h7???: begin 	//ADD Vx, byte
-				opcode = 8;
+				opcode = 21;
+				immediate = instruction[1:0];
+				r1 = registers[instruction[2]];
 			end
 			16'h8??4: begin 	//ADD Vx, Vy
-				opcode = 8;
+				opcode = 22;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'hF?1E: begin 	//ADD I, Vx
-				opcode = 8;
+				opcode = 22;
+				r1 = registers[instruction[2]];
+				r2 = 16;
 			end
 			16'h8??5: begin 	//SUB Vx, Vy
-				opcode = 12;
+				opcode = 24;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??7: begin 	//SUBN Vx, Vy
-				opcode = 14;
+				opcode = 25;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??1: begin 	//OR Vx, Vy
-				opcode = 9;
+				opcode = 26;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??2: begin 	//AND Vx, Vy
-				opcode = 10;
+				opcode = 27;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??3: begin 	//XOR Vx, Vy
-				opcode = 11;
+				opcode = 28;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??6: begin 	//SHR Vx, Vy
-				opcode = 13;
+				opcode = 29;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'h8??E: begin 	//SHL Vx
-				opcode = 15;
+				opcode = 30;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
 			end
 			16'hC???: begin 	//RND Vx, byte
-				opcode = 18;
+				opcode = 31;
+				r1 = registers[instruction[2]];
+				immediate = instruction[1:0];
 			end
 			16'hD???: begin 	//DRW Vx, Vy, nibble
-				opcode = 19;
+				opcode = 32;
+				r1 = registers[instruction[2]];
+				r2 = registers[instruction[1]];
+				immediate = instruction[0];
 			end
 			16'hE?9E: begin 	//SKP Vx
-				opcode = 20;
+				opcode = 33;
+				r1 = registers[instruction[2]];
 			end
 			16'hE?A1: begin 	//SKNP Vx
-				opcode = 21;
+				opcode = 34;
+				r1 = registers[instruction[2]];
 			end
+			default:
+				decode_trap = 1;
 		endcase
 	end
 endmodule
--- new/fetch.sv
+++ new/fetch.sv
@@ -33,18 +33,18 @@ module fetch(
 
 always_ff @(posedge clk) begin
     if(rst) begin
-		//80 for builtin character sprites and 32 for a 16 entry stack
+		//80 for builtin character sprites, 32 for a 16 entry stack
 		program_counter = 112;//should always be even but I won't enfore that/forceably set the LSB to 0
 	end else begin
 		if(branching)
 			program_counter <= branch;
 		else if(offseting)
 			program_counter <= program_counter + 2 + offset;
-		else if(stalled)
+		else if(!stalled)
 			program_counter <= program_counter + 2;
 		else
 			program_counter <= program_counter;//for when waiting on a keypress
-	end
+	end 
 end
 
 
